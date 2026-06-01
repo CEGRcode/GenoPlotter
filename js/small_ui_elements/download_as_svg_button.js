895b3e5145fe97d993b83196782636cfacd9e1cc
jcha40 <sungsoocha97@gmail.com>
2026-06-01 14:09:35 -0400

    Made download svg button prompt user

--- js/small_ui_elements/download_as_svg_button.js
+++ js/small_ui_elements/download_as_svg_button.js
@@ -1,2 +1,31 @@
-d3.select("#download-as-svg").on("click", function() {plotObj.downloadAsSVG()});
-d3.select("#download-minimal-svg").on("click", function() {plotObj.downloadAsSVG(true)})
+d3.select("#download-as-svg").on("click", function() {
+    // Create a prompt to ask if the user wants to include the legend and text labels in the downloaded SVG
+    const prompt = d3.select("body").append("div")
+        .attr("id", "download-svg-prompt")
+    prompt.append("p").text("Include legend and text labels?");
+    const buttonContainer = prompt.append("div")
+            .attr("id", "download-svg-button-container"),
+        includeTextButton = buttonContainer.append("button")
+            .attr("id", "include-text")
+            .text("Yes"),
+        excludeTextButton = buttonContainer.append("button")
+            .attr("id", "exclude-text")
+            .text("No"),
+        cancelDownloadButton = buttonContainer.append("button")
+            .attr("id", "cancel-download")
+            .text("Cancel");
+
+    includeTextButton.on("click", function() {
+        plotObj.downloadAsSVG();
+        prompt.remove()
+    });
+
+    excludeTextButton.on("click", function() {
+        plotObj.downloadAsSVG(true);
+        prompt.remove()
+    });
+
+    cancelDownloadButton.on("click", function() {
+        prompt.remove()
+    })
+})
