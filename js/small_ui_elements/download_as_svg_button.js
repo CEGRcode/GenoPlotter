03c8ef696ed625091e6b9373d3f0b9a4915ffd1f
owlang <oliviawlang@gmail.com>
2026-06-18 00:59:16 -0400

    streamline download svg
    - remove "cancel" button and add red "X" to upper right for a cleaner UI
    - format prompt with darkening overlay on background to better highlight pop-up

--- js/small_ui_elements/download_as_svg_button.js
+++ js/small_ui_elements/download_as_svg_button.js
@@ -1,31 +1,38 @@
 d3.select("#download-as-svg").on("click", function() {
-    // Create a prompt to ask if the user wants to include the legend and text labels in the downloaded SVG
-    const prompt = d3.select("body").append("div")
+    const overlay = d3.select("body").append("div")
+        .attr("id", "download-svg-overlay")
+        .on("click", function() {overlay.remove()});
+    const prompt = overlay.append("div")
         .attr("id", "download-svg-prompt")
-    prompt.append("p").text("Include legend and text labels?");
+        .on("click", function(ev) {ev.stopPropagation()});
+
+    // Add close button
+    prompt.append("i")
+        .classed("remove-icon fa-solid fa-lg fa-times-circle", true)
+        .attr("id", "download-svg-close")
+        .on("click", function() {overlay.remove()});
+
+    // Add header + question
+    prompt.append("h5").text("Download as SVG");
+    prompt.append("p").text("Include legend with text labels?");
+
+    // Add button options for types of SVG download
     const buttonContainer = prompt.append("div")
             .attr("id", "download-svg-button-container"),
         includeTextButton = buttonContainer.append("button")
             .attr("id", "include-text")
-            .text("Include labels"),
+            .classed("panel-action-button", true)
+            .text("Include labels")
+            .on("click", function() {
+                plotObj.downloadAsSVG(true);
+                overlay.remove()
+            }),
         excludeTextButton = buttonContainer.append("button")
             .attr("id", "exclude-text")
-            .text("Plot only"),
-        cancelDownloadButton = buttonContainer.append("button")
-            .attr("id", "cancel-download")
-            .text("Cancel");
-
-    includeTextButton.on("click", function() {
-        plotObj.downloadAsSVG();
-        prompt.remove()
-    });
-
-    excludeTextButton.on("click", function() {
-        plotObj.downloadAsSVG(true);
-        prompt.remove()
-    });
-
-    cancelDownloadButton.on("click", function() {
-        prompt.remove()
-    })
+            .classed("panel-action-button", true)
+            .text("Plot only")
+            .on("click", function() {
+                plotObj.downloadAsSVG();
+                overlay.remove()
+            });
 })
